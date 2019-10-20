a6ef0d93454c50279fae6b670c314bf7fd26f395
Brian Rutledge <bhrutledge@gmail.com>
2019-10-20 11:32:59 -0400

    Document check_version

--- scripts/check_version.py
+++ scripts/check_version.py
@@ -2,11 +2,28 @@ import sys
 
 from setuptools_scm import get_version
 
-version = get_version()
-print("Version: " + version)
 
-if "+" in version:
-    sys.exit(
-        "ERROR: Invalid version. "
-        "Make sure the working tree is clean, then tag a new version."
-    )
+def main():
+    """Validate the package version for PyPI.
+
+    If the version contains a local identifier, return an error message suitable for
+    sys.exit. Otherwise, return None.
+
+    See also:
+    https://github.com/pypa/setuptools_scm/issues/365
+    https://github.com/pypa/twine/issues/430
+    https://www.python.org/dev/peps/pep-0440/#local-version-identifiers
+
+    """
+    version = get_version()
+    print("Version: " + version)
+
+    if "+" in version:
+        return (
+            "ERROR: Invalid version. "
+            "Make sure the working tree is clean, then tag a new version."
+        )
+
+
+if __name__ == "__main__":
+    sys.exit(main())
