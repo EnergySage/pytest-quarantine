4641c955cb30ce935728573e27e7f59fbdcf4455
Brian Rutledge <brian@bhrutledge.com>
2019-11-03 06:06:43 -0500

    Add plugin source from EnergySage

--- src/pytest_quarantine.py
+++ src/pytest_quarantine.py
@@ -1,21 +1,80 @@
 import pytest
 
 
+class QuarantinePlugin(object):
+    """Save a list of failing tests to be marked as xfail on future test runs."""
+
+    # TODO: Guarantee this is opened from pytest's root dir
+    DEFAULT_QUARANTINE = "quarantine.txt"
+
+    def __init__(self, config):
+        self.quarantine = config.getoption("quarantine")
+        self.save_quarantine = config.getoption("save_quarantine")
+        self.nodeids = set()
+
+    @classmethod
+    def addoptions(cls, parser):
+        """Add command line options for the quarantine."""
+        group = parser.getgroup("quarantine")
+
+        group.addoption(
+            "--save-quarantine",
+            nargs="?",
+            const=cls.DEFAULT_QUARANTINE,
+            metavar="PATH",
+            help="Write failing tests to %(metavar)s (default: %(const)s)",
+        )
+
+        group.addoption(
+            "--quarantine",
+            nargs="?",
+            const=cls.DEFAULT_QUARANTINE,
+            metavar="PATH",
+            help="Mark tests listed in %(metavar)s with `xfail` (default: %(const)s)",
+        )
+
+    def pytest_report_header(self, config):
+        """Display configuration at runtime."""
+        options = [
+            (option, getattr(self, option))
+            for option in ["quarantine", "save_quarantine"]
+        ]
+        return ["{}: {}".format(option, value) for option, value in options if value]
+
+    def pytest_runtestloop(self, session):
+        """Read the ID's of quarantined tests from a file."""
+        if not self.quarantine:
+            return
+
+        with open(self.quarantine) as f:
+            for nodeid in f:
+                self.nodeids.add(nodeid.strip())
+
+    def pytest_runtest_setup(self, item):
+        """Mark a test as `xfail` if its ID is in the quarantine."""
+        if self.quarantine and item.nodeid in self.nodeids:
+            item.add_marker(pytest.mark.xfail(reason="Quarantined"))
+
+    def pytest_runtest_logreport(self, report):
+        """Save the ID of a failed test to the quarantine."""
+        if not report.passed:
+            self.nodeids.add(report.nodeid)
+
+    def pytest_sessionfinish(self, session):
+        """Write the ID's of quarantined tests to a file."""
+        if not (self.save_quarantine and self.nodeids):
+            return
+
+        with open(self.save_quarantine, "w") as f:
+            f.writelines(nodeid + "\n" for nodeid in sorted(self.nodeids))
+
+
 def pytest_addoption(parser):
-    """Add the `--foo` command-line option."""
-    group = parser.getgroup("quarantine")
-    group.addoption(
-        "--foo",
-        action="store",
-        dest="dest_foo",
-        default="2019",
-        help='Set the value for the fixture "bar".',
-    )
-
-    parser.addini("HELLO", "Dummy pytest.ini setting")
+    """Add command line options."""
+    QuarantinePlugin.addoptions(parser)
 
 
-@pytest.fixture
-def bar(request):
-    """Return the value of the `foo` option."""
-    return request.config.option.dest_foo
+# TODO: ValueError: Plugin already registered: quarantine
+# def pytest_configure(config):
+#     """Register the plugin."""
+#     config.pluginmanager.register(QuarantinePlugin(config), "quarantine")
--- tests/test_quarantine.py
+++ tests/test_quarantine.py
@@ -1,58 +1,7 @@
-def test_bar_fixture(testdir):
-    """Make sure that pytest accepts our fixture."""
-    # create a temporary pytest test module
-    testdir.makepyfile(
-        """
-        def test_sth(bar):
-            assert bar == "europython2015"
-        """
-    )
-
-    # run pytest with the following cmd args
-    result = testdir.runpytest("--foo=europython2015", "-v")
-
-    # fnmatch_lines does an assertion internally
-    result.stdout.fnmatch_lines(["*::test_sth PASSED*"])
-
-    # make sure that that we get a '0' exit code for the testsuite
-    assert result.ret == 0
-
-
-def test_help_message(testdir):
-    """Make sure there's help text for `--foo`."""
+def test_quarantine_options(testdir):
+    """Make sure there's help text."""
     result = testdir.runpytest("--help")
-    # fnmatch_lines does an assertion internally
+
     result.stdout.fnmatch_lines(
-        ["quarantine:", '*--foo=DEST_FOO*Set the value for the fixture "bar".']
+        ["quarantine:", "*--save-quarantine=*", "*--quarantine=*"]
     )
-
-
-def test_hello_ini_setting(testdir):
-    """Demonstrate testing configuration files."""
-    testdir.makeini(
-        """
-        [pytest]
-        HELLO = world
-        """
-    )
-
-    testdir.makepyfile(
-        """
-        import pytest
-
-        @pytest.fixture
-        def hello(request):
-            return request.config.getini('HELLO')
-
-        def test_hello_world(hello):
-            assert hello == 'world'
-        """
-    )
-
-    result = testdir.runpytest("-v")
-
-    # fnmatch_lines does an assertion internally
-    result.stdout.fnmatch_lines(["*::test_hello_world PASSED*"])
-
-    # make sure that that we get a '0' exit code for the testsuite
-    assert result.ret == 0
