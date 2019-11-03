56660e962ed8bfacd28a7bed945f8aa3747b551f
Brian Rutledge <brian@bhrutledge.com>
2019-11-03 06:52:07 -0500

    Add initial functional tests

--- tests/test_quarantine.py
+++ tests/test_quarantine.py
@@ -1,7 +1,53 @@
-def test_quarantine_options(testdir):
-    """Make sure there's help text."""
+import pytest
+
+
+def test_options(testdir):
     result = testdir.runpytest("--help")
 
     result.stdout.fnmatch_lines(
         ["quarantine:", "*--save-quarantine=*", "*--quarantine=*"]
     )
+
+
+@pytest.fixture
+def failing_tests(testdir):
+    testdir.makepyfile(
+        """
+        import pytest
+
+        @pytest.fixture
+        def error():
+            assert False
+
+        def test_error(error):
+            assert True
+
+        def test_failure():
+            assert False
+
+        def test_pass():
+            assert True
+        """
+    )
+
+
+def test_save_quarantine(testdir, failing_tests):
+    result = testdir.runpytest("--save-quarantine")
+
+    result.stdout.fnmatch_lines(["save_quarantine: quarantine.txt"])
+    result.assert_outcomes(passed=1, failed=1, error=1)
+
+    assert testdir.tmpdir.join("quarantine.txt").read() == (
+        "test_save_quarantine.py::test_error\ntest_save_quarantine.py::test_failure\n"
+    )
+
+
+def test_use_quarantine(testdir, failing_tests):
+    testdir.tmpdir.join("quarantine.txt").write(
+        "test_use_quarantine.py::test_error\ntest_use_quarantine.py::test_failure\n"
+    )
+
+    result = testdir.runpytest("--quarantine")
+
+    result.stdout.fnmatch_lines(["quarantine: quarantine.txt"])
+    result.assert_outcomes(passed=1, xfailed=2)
